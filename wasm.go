0a9e9fc1577bc3c1090798601fc9e463926769fd
Justin Clift <justin@postgresql.org>
2019-04-27 23:07:27 +1000

    Removed the fmt package
    The using strconv for integer conversion to strings seems ok, and
    barely adds to the package size.

--- wasm.go
+++ wasm.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
+	"strconv"
 	"syscall/js"
 )
 
 var (
-	ctx js.Value
+	ctx           js.Value
 	height, width int
 )
 
@@ -16,14 +16,10 @@ func main() {
 
 //go:export clearCanvas
 func clearCanvas() {
-
-	fmt.Println("Clearing canvas...")
-
 	// Initialise doc
 	doc := js.Global().Get("document")
 	canvasEl := doc.Call("getElementById", "mycanvas")
 	width = doc.Get("body").Get("clientWidth").Int()
-	fmt.Printf("Width: %v", width)
 	height = doc.Get("body").Get("clientHeight").Int()
 	canvasEl.Call("setAttribute", "width", width)
 	canvasEl.Call("setAttribute", "height", height)
@@ -33,14 +29,10 @@ func clearCanvas() {
 	// Clear the background
 	ctx.Set("fillStyle", "lightgrey")
 	ctx.Call("fillRect", 0, 0, width, height)
-
-	fmt.Println("Canvas cleared")
 }
 
 //go:export drawLine
 func drawLine() {
-	fmt.Println("Drawing a line")
-
 	// Generate random start and end points
 	startX := rand.Intn(width)
 	startY := rand.Intn(height)
@@ -52,11 +44,10 @@ func drawLine() {
 	colG := rand.Intn(255)
 	colB := rand.Intn(255)
 
-	ctx.Set("strokeStyle", fmt.Sprintf("rgb(%d, %d, %d)", colR, colG, colB))
+	ctx.Set("strokeStyle", "rgb("+strconv.Itoa(colR)+", "+strconv.Itoa(colG)+", "+strconv.Itoa(colB)+")")
 	ctx.Set("lineWidth", "5")
 	ctx.Call("beginPath")
 	ctx.Call("moveTo", startX, startY)
 	ctx.Call("lineTo", endX, endY)
 	ctx.Call("stroke")
-	fmt.Println("Line drawn")
 }
