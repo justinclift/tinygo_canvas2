7ed55b272bdb1dafeb4df5ed70bfb329b44cd81f
Justin Clift <justin@postgresql.org>
2019-04-28 15:40:58 +1000

    Add my old hand written matrix code from wasmGraph5
    Compiles ok as-is, without any porting needed. :)

--- wasm.go
+++ wasm.go
@@ -7,6 +7,16 @@ import (
 	"syscall/js"
 )
 
+type matrix []float64
+
+type Point struct {
+	Label      string
+	LabelAlign string
+	X          float64
+	Y          float64
+	Z          float64
+}
+
 var (
 	canvasEl, ctx, doc js.Value
 	graphWidth         float64
@@ -90,6 +100,49 @@ func mouseDownHandler(clientX int, clientY int) {
 	ctx.Call("stroke")
 }
 
+// Multiplies one matrix by another
+func matrixMult(opMatrix matrix, m matrix) (resultMatrix matrix) {
+	top0 := m[0]
+	top1 := m[1]
+	top2 := m[2]
+	top3 := m[3]
+	upperMid0 := m[4]
+	upperMid1 := m[5]
+	upperMid2 := m[6]
+	upperMid3 := m[7]
+	lowerMid0 := m[8]
+	lowerMid1 := m[9]
+	lowerMid2 := m[10]
+	lowerMid3 := m[11]
+	bot0 := m[12]
+	bot1 := m[13]
+	bot2 := m[14]
+	bot3 := m[15]
+
+	resultMatrix = matrix{
+		(opMatrix[0] * top0) + (opMatrix[1] * upperMid0) + (opMatrix[2] * lowerMid0) + (opMatrix[3] * bot0), // 1st col, top
+		(opMatrix[0] * top1) + (opMatrix[1] * upperMid1) + (opMatrix[2] * lowerMid1) + (opMatrix[3] * bot1), // 2nd col, top
+		(opMatrix[0] * top2) + (opMatrix[1] * upperMid2) + (opMatrix[2] * lowerMid2) + (opMatrix[3] * bot2), // 3rd col, top
+		(opMatrix[0] * top3) + (opMatrix[1] * upperMid3) + (opMatrix[2] * lowerMid3) + (opMatrix[3] * bot3), // 4th col, top
+
+		(opMatrix[4] * top0) + (opMatrix[5] * upperMid0) + (opMatrix[6] * lowerMid0) + (opMatrix[7] * bot0), // 1st col, upper middle
+		(opMatrix[4] * top1) + (opMatrix[5] * upperMid1) + (opMatrix[6] * lowerMid1) + (opMatrix[7] * bot1), // 2nd col, upper middle
+		(opMatrix[4] * top2) + (opMatrix[5] * upperMid2) + (opMatrix[6] * lowerMid2) + (opMatrix[7] * bot2), // 3rd col, upper middle
+		(opMatrix[4] * top3) + (opMatrix[5] * upperMid3) + (opMatrix[6] * lowerMid3) + (opMatrix[7] * bot3), // 4th col, upper middle
+
+		(opMatrix[8] * top0) + (opMatrix[9] * upperMid0) + (opMatrix[10] * lowerMid0) + (opMatrix[11] * bot0), // 1st col, lower middle
+		(opMatrix[8] * top1) + (opMatrix[9] * upperMid1) + (opMatrix[10] * lowerMid1) + (opMatrix[11] * bot1), // 2nd col, lower middle
+		(opMatrix[8] * top2) + (opMatrix[9] * upperMid2) + (opMatrix[10] * lowerMid2) + (opMatrix[11] * bot2), // 3rd col, lower middle
+		(opMatrix[8] * top3) + (opMatrix[9] * upperMid3) + (opMatrix[10] * lowerMid3) + (opMatrix[11] * bot3), // 4th col, lower middle
+
+		(opMatrix[12] * top0) + (opMatrix[13] * upperMid0) + (opMatrix[14] * lowerMid0) + (opMatrix[15] * bot0), // 1st col, bottom
+		(opMatrix[12] * top1) + (opMatrix[13] * upperMid1) + (opMatrix[14] * lowerMid1) + (opMatrix[15] * bot1), // 2nd col, bottom
+		(opMatrix[12] * top2) + (opMatrix[13] * upperMid2) + (opMatrix[14] * lowerMid2) + (opMatrix[15] * bot2), // 3rd col, bottom
+		(opMatrix[12] * top3) + (opMatrix[13] * upperMid3) + (opMatrix[14] * lowerMid3) + (opMatrix[15] * bot3), // 4th col, bottom
+	}
+	return resultMatrix
+}
+
 // Renders one frame of the animation
 func renderFrame() {
 	// Handle window resizing
@@ -133,4 +186,89 @@ func renderFrame() {
 		ctx.Call("lineTo", graphWidth-border, i+step)
 		ctx.Call("stroke")
 	}
+}
+
+// Rotates a transformation matrix around the X axis by the given degrees
+func rotateAroundX(m matrix, degrees float64) matrix {
+	rad := (math.Pi / 180) * degrees // The Go math functions use radians, so we convert degrees to radians
+	rotateXMatrix := matrix{
+		1, 0, 0, 0,
+		0, math.Cos(rad), -math.Sin(rad), 0,
+		0, math.Sin(rad), math.Cos(rad), 0,
+		0, 0, 0, 1,
+	}
+	return matrixMult(rotateXMatrix, m)
+}
+
+// Rotates a transformation matrix around the Y axis by the given degrees
+func rotateAroundY(m matrix, degrees float64) matrix {
+	rad := (math.Pi / 180) * degrees // The Go math functions use radians, so we convert degrees to radians
+	rotateYMatrix := matrix{
+		math.Cos(rad), 0, math.Sin(rad), 0,
+		0, 1, 0, 0,
+		-math.Sin(rad), 0, math.Cos(rad), 0,
+		0, 0, 0, 1,
+	}
+	return matrixMult(rotateYMatrix, m)
+}
+
+// Rotates a transformation matrix around the Z axis by the given degrees
+func rotateAroundZ(m matrix, degrees float64) matrix {
+	rad := (math.Pi / 180) * degrees // The Go math functions use radians, so we convert degrees to radians
+	rotateZMatrix := matrix{
+		math.Cos(rad), -math.Sin(rad), 0, 0,
+		math.Sin(rad), math.Cos(rad), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1,
+	}
+	return matrixMult(rotateZMatrix, m)
+}
+
+// Scales a transformation matrix by the given X, Y, and Z values
+func scale(m matrix, x float64, y float64, z float64) matrix {
+	scaleMatrix := matrix{
+		x, 0, 0, 0,
+		0, y, 0, 0,
+		0, 0, z, 0,
+		0, 0, 0, 1,
+	}
+	return matrixMult(scaleMatrix, m)
+}
+
+// Transform the XYZ co-ordinates using the values from the transformation matrix
+func transform(m matrix, p Point) (t Point) {
+	top0 := m[0]
+	top1 := m[1]
+	top2 := m[2]
+	top3 := m[3]
+	upperMid0 := m[4]
+	upperMid1 := m[5]
+	upperMid2 := m[6]
+	upperMid3 := m[7]
+	lowerMid0 := m[8]
+	lowerMid1 := m[9]
+	lowerMid2 := m[10]
+	lowerMid3 := m[11]
+	//bot0 := m[12] // The fourth row values can be ignored for 3D matrices
+	//bot1 := m[13]
+	//bot2 := m[14]
+	//bot3 := m[15]
+
+	t.Label = p.Label
+	t.LabelAlign = p.LabelAlign
+	t.X = (top0 * p.X) + (top1 * p.Y) + (top2 * p.Z) + top3
+	t.Y = (upperMid0 * p.X) + (upperMid1 * p.Y) + (upperMid2 * p.Z) + upperMid3
+	t.Z = (lowerMid0 * p.X) + (lowerMid1 * p.Y) + (lowerMid2 * p.Z) + lowerMid3
+	return
+}
+
+// Translates (moves) a transformation matrix by the given X, Y and Z values
+func translate(m matrix, translateX float64, translateY float64, translateZ float64) matrix {
+	translateMatrix := matrix{
+		1, 0, 0, translateX,
+		0, 1, 0, translateY,
+		0, 0, 1, translateZ,
+		0, 0, 0, 1,
+	}
+	return matrixMult(translateMatrix, m)
 }
